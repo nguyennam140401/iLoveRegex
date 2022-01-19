f908c85dcd3cdc6ab76bf06358e51b323bc820d4
nam140401 <nguyennam140401@gmail.com>
2022-01-19 16:31:10 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,47 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+    const inputRegex = useRef()
+
     const [strRegex, setStrRegex] = useState('')
-    const [strTest, setStrTest] = useState([
-        {
-            text: '+919367788755',
-            status: false,
-        },
-        {
-            text: '8989829304',
-            status: false,
-        },
-        {
-            text: '123765',
-            status: false,
-        },
-        {
-            text: '+982',
-            status: false,
-        },
-    ])
+    const [textMatch, setTextMatch] = useState('')
+    const [strTest, setStrTest] = useState([])
     const checkRegex = (obj, regex) => {
-        console.log(obj)
         return { ...obj, status: regex.test(obj.text) }
     }
+    useEffect(() => {
+        console.log(strTest)
+    })
     useEffect(() => {
         let arrTest = JSON.parse(JSON.stringify(strTest))
         const regex = new RegExp(strRegex)
         arrTest = arrTest.map((x) => checkRegex(x, regex))
-        // console.log(arrTest)
         setStrTest(arrTest)
     }, [strRegex])
+    useEffect(() => {
+        console.log(textMatch)
+        const str = textMatch
+        const regex = new RegExp(strRegex)
+        setStrTest(
+            str.split(/\r?\n/).map((x) => {
+                return { text: x, status: regex.test(x) }
+            })
+        )
+    }, [textMatch])
     const handleChangeInput = (e) => {
         const value = e.target.value
         setStrRegex(value)
+    }
+    const handleChangeTextarea = (e) => {
+        const value = e.target.value
+        setTextMatch(value)
+    }
+    const copyRegex = () => {
+        inputRegex.current.select()
+        inputRegex.current.setSelectionRange(0, 99999)
+        navigator.clipboard.writeText(inputRegex.current.value)
+        alert('Chep thanh cong')
     }
     return (
         <div id="app">
@@ -50,12 +57,22 @@ function App() {
                             name="strRegex"
                             value={strRegex}
                             onChange={handleChangeInput}
+                            autoComplete="off"
+                            ref={inputRegex}
                         />
-                        <button>Copy</button>
+                        <button onClick={copyRegex}>Copy</button>
                     </div>
                     <div className="result">
-                        <textarea name="textMatch"></textarea>
-                        {/* <div className="match">
+                        <textarea
+                            name="textMatch"
+                            value={textMatch}
+                            onChange={handleChangeTextarea}
+                            spellCheck="false"
+                            autoComplete="off"
+                            autoCorrect="off"
+                            autoCapitalize="off"
+                        ></textarea>
+                        <div className="match">
                             {strTest.map((_, idx) => {
                                 return (
                                     <span
@@ -66,7 +83,7 @@ function App() {
                                     </span>
                                 )
                             })}
-                        </div> */}
+                        </div>
                     </div>
                 </div>
             </div>
